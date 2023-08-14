9e5a4c028794a09a6caccdca00ff0c3723a9b528
rolroralra <rolroralra@gmail.com>
2023-08-14 23:27:20 +0900

    Initial commit

---
+++ src/main/java/hello/springmvc/SpringMvcPart12Application.java
@@ -0,0 +1,12 @@
+package hello.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcPart12Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcPart12Application.class, args);
+    }
+
+}
---
+++ src/main/java/hello/springmvc/controller/LogTestController.java
@@ -0,0 +1,27 @@
+package hello.springmvc.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/log-test")
+@Slf4j
+public class LogTestController {
+    @GetMapping
+    public String logTest() {
+        String name = "Spring";
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        log.debug("String concat log=" + name);
+
+        return HttpStatus.OK.toString();
+    }
+}
